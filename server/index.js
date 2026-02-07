c9d918072d0f7ce7b3cc5f3f25f9b6059f658e42
SifanGeremu <wins33975@gmail.com>
2026-02-07 22:23:41 +0300

    feat(backend): allow origin https://stack-step.vercel.app

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://stack-step.vercel.app/login"],
+    origin: ["https://stack-step.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
